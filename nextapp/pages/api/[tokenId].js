2d0ff4f8baa94b58d5c620efa65c4cd0c7dd0875
Web3Wiz <wizcraftsol@gmail.com>
2022-11-01 16:04:43 +0500

    Update [tokenId].js

--- nextapp/pages/api/[tokenId].js
+++ nextapp/pages/api/[tokenId].js
@@ -1,12 +1,12 @@
 export default function handler(req, res) {
 
     const tokenId = req.query.tokenId;
-    const imageUrl = "https://github.com/Web3Wiz/NFTCollection/tree/main/nextapp/public/cryptodevs/";
+    const imageUrl = "https://raw.githubusercontent.com/Web3Wiz/NFTCollection/main/nextapp/public/cryptodevs/";
 
 
     res.status(200).json({
         name: 'Crypto Dev #' + tokenId,
         description: "Crypto Dev is a NFT collection for developers in Crypto",
-        image: imageUrl + tokenId + ".svg"
+        image: imageUrl + (parseInt(tokenId)-1) + ".svg"
     })
 }
