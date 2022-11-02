5dced116ad267af0e43b29f65cbee7fbe7bb2a3d
Web3Wiz <wizcraftsol@gmail.com>
2022-11-02 18:26:01 +0500

    Update index.js

--- nextapp/pages/index.js
+++ nextapp/pages/index.js
@@ -1,16 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useState, useEffect, useRef } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useState, useEffect, useRef } from "react";
 
-import { providers, Contract, utils } from 'ethers';
-import Web3Modal from 'web3modal';
-import { CONTRACT_ADDRESS, CONTRACT_ABI } from '../constants';
-
-
+import { providers, Contract, utils } from "ethers";
+import Web3Modal from "web3modal";
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../constants";
 
 export default function Home() {
-
   const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
   const [walletConnected, setWlletConnected] = useState(false);
   const [alreadyMinted, setAlreadyMinted] = useState(false);
@@ -21,52 +18,47 @@ export default function Home() {
 
   const web3ModalRef = useRef();
 
-
   useEffect(() => {
-
     if (!walletConnected) {
       web3ModalRef.current = new Web3Modal({
-        network: 'goerli',
+        network: "goerli",
         providerOptions: {},
-        disabledInjectedProvider: false
+        disabledInjectedProvider: false,
       });
     }
     connectWallet();
     getMintedTokensIDs();
 
-    if (IsPresaleStarted())
-      IsPresaleEnded();
-
+    if (IsPresaleStarted()) IsPresaleEnded();
 
     const periodicCalls = setInterval(async () => {
       const _isPresaleStarted = await IsPresaleStarted();
       if (_isPresaleStarted) {
         const _isPresaleEnded = await IsPresaleEnded();
-        if (_isPresaleEnded)
-          clearInterval(periodicCalls);
+        if (_isPresaleEnded) clearInterval(periodicCalls);
       }
     }, 5000);
     setInterval(async () => {
       getMintedTokensIDs();
     }, 5000);
 
-
-    if (isContractOwner())
-      renderWithdrawButton();
+    if (isContractOwner()) renderWithdrawButton();
 
     hasAddressAlreadyMinted();
-
-  }, [walletConnected])
+  }, [walletConnected]);
 
   useEffect(() => {
     renderNFTButton();
-  }, [alreadyMinted, isPresaleStarted, isPresaleEnded, isLoading, alreadyMinted])
-
+  }, [
+    alreadyMinted,
+    isPresaleStarted,
+    isPresaleEnded,
+    isLoading,
+    alreadyMinted,
+  ]);
 
   const connectWallet = async () => {
-
     try {
-
       setIsLoading(true);
 
       const provider = await getProviderOrSigner();
@@ -74,19 +66,14 @@ export default function Home() {
         setWlletConnected(true);
       }
 
-
       setIsLoading(false);
-
     } catch (error) {
       setIsLoading(false);
       console.error(error);
     }
-
-
-  }
+  };
 
   const startPresale = async () => {
-
     try {
       const signer = await getProviderOrSigner(true);
       const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
@@ -96,44 +83,34 @@ export default function Home() {
       await tx.wait();
       setIsLoading(false);
       setIsPresaleStarted(true);
-
     } catch (error) {
       setIsLoading(false);
       console.error(error);
     }
-
-  }
+  };
   const IsPresaleStarted = async () => {
-
     try {
       const provider = await getProviderOrSigner();
       const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
       const _isPresaleStarted = await contract.presaleStarted();
       setIsPresaleStarted(_isPresaleStarted);
-
     } catch (error) {
       console.error(error);
     }
-
-  }
+  };
   const IsPresaleEnded = async () => {
-
     try {
       const provider = await getProviderOrSigner();
       const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
       const _isPresaleEnded = await contract.isPresaleEnded();
       setIsPresaleEnded(_isPresaleEnded);
-
     } catch (error) {
       console.error(error);
     }
-
-  }
-
+  };
 
   const presaleMint = async () => {
     try {
-
       if (await hasAddressAlreadyMinted()) {
         window.alert("Sender address has already minted the token.");
         return false;
@@ -153,10 +130,9 @@ export default function Home() {
       setIsLoading(false);
       console.error(error);
     }
-  }
+  };
   const publicMint = async () => {
     try {
-
       if (await hasAddressAlreadyMinted()) {
         window.alert("Sender address has already minted the token.");
         return false;
@@ -176,23 +152,19 @@ export default function Home() {
       setIsLoading(false);
       console.error(error);
     }
-  }
+  };
   const getMintedTokensIDs = async () => {
-
     try {
       const provider = await getProviderOrSigner();
       const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
       const _tokenIdsMinted = await contract.mintedTokensIDs();
       setTokenIdsMinted(_tokenIdsMinted.toString());
-
     } catch (error) {
       console.error(error);
     }
-
-  }
+  };
 
   const hasAddressAlreadyMinted = async () => {
-
     try {
       const signer = await getProviderOrSigner(true);
       const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
@@ -203,14 +175,12 @@ export default function Home() {
     } catch (error) {
       console.error(error);
     }
-
-  }
+  };
 
   const getProviderOrSigner = async (needSigner = false) => {
     try {
       const currentProvider = await web3ModalRef.current.connect();
       const web3Provider = new providers.Web3Provider(currentProvider);
-
 
       const { chainId } = await web3Provider.getNetwork();
       console.log("getProviderOrSigner --> Chain ID is", chainId);
@@ -222,15 +192,12 @@ export default function Home() {
       if (needSigner) {
         return web3Provider.getSigner();
       }
-      
-      return web3Provider;
 
+      return web3Provider;
     } catch (error) {
       console.error(error);
     }
-
-  }
-
+  };
 
   const isContractOwner = async () => {
     try {
@@ -241,12 +208,13 @@ export default function Home() {
       const signer = await getProviderOrSigner(true);
       const currentUserAddress = await signer.getAddress();
 
-      setIsOwner(currentUserAddress.toLowerCase() === contractOwnerAddress.toLowerCase());
-
+      setIsOwner(
+        currentUserAddress.toLowerCase() === contractOwnerAddress.toLowerCase()
+      );
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   const contractHasAmount = async () => {
     try {
       const provider = await getProviderOrSigner();
@@ -255,8 +223,7 @@ export default function Home() {
     } catch (error) {
       console.error(error);
     }
-
-  }
+  };
 
   const withdrawAmount = async () => {
     try {
@@ -265,47 +232,43 @@ export default function Home() {
         const signer = await getProviderOrSigner(true);
         const contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
         await contract.withdraw();
-        window.alert("Contract amount withdrawn successfully and moved to the owner's wallet");
+        window.alert(
+          "Contract amount withdrawn successfully and moved to the owner's wallet"
+        );
+      } else {
+        window.alert(
+          "Contract's balance is empty. There is no amount to be withdrawn"
+        );
       }
-      else {
-        window.alert("Contract's balance is empty. There is no amount to be withdrawn");
-      }
-
-
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   const renderWithdrawButton = () => {
-
     if (isOwner)
       return (
-
-        <button className={styles.button} onClick={withdrawAmount}>Withdraw Contract Amount 💰 </button>
-
+        <button className={styles.button} onClick={withdrawAmount}>
+          Withdraw Contract Amount 💰{" "}
+        </button>
       );
-  }
+  };
   const renderNFTButton = () => {
-
     if (walletConnected) {
-
       if (alreadyMinted) {
         return (
           <div className={styles.description}>
             Thanks for minting the CryptoDev NFT.
           </div>
         );
-      }
-      else if (isLoading) {
-        return (
-          <button className={styles.button}>Processing ...</button>
-        );
-      }
-      else {
+      } else if (isLoading) {
+        return <button className={styles.button}>Processing ...</button>;
+      } else {
         if (!isPresaleStarted) {
           if (isOwner)
             return (
-              <button className={styles.button} onClick={startPresale}>Start Presale 🚀</button>
+              <button className={styles.button} onClick={startPresale}>
+                Start Presale 🚀
+              </button>
             );
           else
             return (
@@ -313,38 +276,38 @@ export default function Home() {
                 Presale hasnt started. Please check back soon!
               </div>
             );
-        }
-        else {
-
+        } else {
           if (!isPresaleEnded) {
             return (
               <div>
                 <div className={styles.description}>
-                  Presale has started!!! If your address is whitelisted, Mint a Crypto Dev 🥳
+                  Presale has started!!! If your address is whitelisted, Mint a
+                  Crypto Dev 🥳
                 </div>
-                <button className={styles.button} onClick={presaleMint}>Presale mint 🚀</button>
+                <button className={styles.button} onClick={presaleMint}>
+                  Presale mint 🚀
+                </button>
               </div>
             );
-          }
-          else {
+          } else {
             return (
-              <button className={styles.button} onClick={publicMint}>Public mint 🚀</button>
+              <button className={styles.button} onClick={publicMint}>
+                Public mint 🚀
+              </button>
             );
           }
         }
       }
-    }
-    else if (isLoading) {
+    } else if (isLoading) {
+      return <button className={styles.button}>Processing ...</button>;
+    } else {
       return (
-        <button className={styles.button}>Processing ...</button>
+        <button className={styles.button} onClick={connectWallet}>
+          Connect Wallet
+        </button>
       );
     }
-    else {
-      return (
-        <button className={styles.button} onClick={connectWallet}>Connect Wallet</button>
-      );
-    }
-  }
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -355,32 +318,25 @@ export default function Home() {
 
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>
-            Welcome to NFT Collection
-          </h1>
+          <h1 className={styles.title}>Welcome to NFT Collection</h1>
           <div className={styles.description}>
             Its an NFT collection for developers in Crypto.
           </div>
           <div className={styles.description}>
             {tokenIdsMinted}/20 have been minted
           </div>
-          <div className={styles.description}>
-            {renderNFTButton()}
-          </div>
-          <div className={styles.description}>
-            {renderWithdrawButton()}
-          </div>
+          <div className={styles.description}>{renderNFTButton()}</div>
+          <div className={styles.description}>{renderWithdrawButton()}</div>
         </div>
 
         <div>
           <img className={styles.image} src="./cryptodevs/0.svg" />
         </div>
-
       </div>
 
       <footer className={styles.footer}>
         Made with &#10084; by Kazim&#169;
       </footer>
     </div>
-  )
+  );
 }
